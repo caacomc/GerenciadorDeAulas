82037787c9a8bf3325f80f5372078ab470d5b5c6
caroline.campos <caacomc@gmail.com>
2026-04-21 16:26:00 -0300

    alteração na extensão de tela e criação da pagina de gerenciamento das aulas

--- GerenciadorDeAulas/Models/Aula.cs
+++ GerenciadorDeAulas/Models/Aula.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace GerenciadorDeAulas.Models
 {
-    internal class Aula
+    public enum StatusHorario
     {
+        Livre,
+        Ocupado
+    }
+
+    public class Horario
+    {
+        public int Id { get; set; }
+
+        public string HorarioTexto { get; set; }
+
+        public StatusHorario Status { get; set; }
+
+        public string Professor { get; set; }
+
+        public string Disciplina { get; set; }
+
+        public string Sala { get; set; }
+
+        public string DiaSemana { get; set; }
     }
 }
--- GerenciadorDeAulas/Views/Calendario.xaml.cs
+++ GerenciadorDeAulas/Views/Calendario.xaml.cs
@@ -19,6 +19,42 @@ public partial class Calendario : ContentPage
         {
             lblEventos.Text = "Férias escolares 📚";
         }
+        else if (data.Day == 8 && data.Month == 3)
+        {
+            lblEventos.Text = "Dia Internacional das Mulheres 🌹";
+        }
+        else if (data.Day == 21 && data.Month == 4)
+        {
+            lblEventos.Text = "Feriado Tiradentes";
+        }
+        else if (data.Day == 1 && data.Month == 5)
+        {
+            lblEventos.Text = "Dia Mundial do Trabalho ⚒️";
+        }
+        else if (data.Day == 7 && data.Month == 9)
+        {
+            lblEventos.Text = "Independência do Brasil";
+        }
+        else if (data.Day == 12 && data.Month == 10)
+        {
+            lblEventos.Text = "Dia de Nossa Senhora Aparecida 🙏❤️";
+        }
+        else if (data.Day == 15 && data.Month == 10)
+        {
+            lblEventos.Text = "Dia dos Professores 👨‍🏫";
+        }
+        else if (data.Day == 2 && data.Month == 11)
+        {
+            lblEventos.Text = "Finados";
+        }
+        else if (data.Day == 15 && data.Month == 11)
+        {
+            lblEventos.Text = "Proclamação da República";
+        }
+        else if (data.Day == 20 && data.Month == 11)
+        {
+            lblEventos.Text = "Dia Nacional de Zumbi e da Consciência Negra";
+        }
         else if (data.Day == 25 && data.Month == 12)
         {
             lblEventos.Text = "Natal 🎄";
