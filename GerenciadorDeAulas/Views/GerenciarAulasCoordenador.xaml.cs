83f96ebee5d2a9244ec36db25cf417a72f268f3c
Manoelly Soares Alves de França <manoellysoares2020@gmail.com>
2026-04-23 18:21:59 -0300

    Pagina Coordenador

---
+++ GerenciadorDeAulas/Views/GerenciarAulasCoordenador.xaml.cs
@@ -0,0 +1,55 @@
+using GerenciadorDeAulas.Models;
+using System.Collections.ObjectModel;
+namespace GerenciadorDeAulas.Views;
+
+public partial class GerenciarAulasPage : ContentPage
+{
+    ObservableCollection<Horario> horarios = new();
+    public GerenciarAulasPage()
+	{
+		InitializeComponent();
+        CarregarHorarios();
+        gradeHorarios.ItemsSource = horarios;
+    }
+
+    void CarregarHorarios()
+    {
+        horarios.Add(new Horario
+        {
+            HorarioTexto = "08:00",
+            Status = StatusHorario.Livre
+        });
+
+        horarios.Add(new Horario
+        {
+            HorarioTexto = "09:00",
+            Status = StatusHorario.Ocupado
+        });
+
+        horarios.Add(new Horario
+        {
+            HorarioTexto = "10:00",
+            Status = StatusHorario.Livre
+        });
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlertAsync ("Selecionado", "Horário escolhido!", "OK");
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        await DisplayAlertAsync ("Adicionar", "Abrir tela de cadastro", "OK");
+    }
+
+    private async void Button_Clicked_2(object sender, EventArgs e)
+    {
+        await DisplayAlertAsync ("Remover", "Remover aula selecionada", "OK");
+    }
+
+    private void Button_Clicked_3(object sender, EventArgs e)
+    {
+
+    }
+}
