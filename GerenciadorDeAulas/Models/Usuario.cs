ec7679248cc1d34861c653f712b58e0156e62df7
Manoelly Soares Alves de França <manoellysoares2020@gmail.com>
2026-04-23 14:30:34 -0300

    teste

--- GerenciadorDeAulas/Models/Usuario.cs
+++ GerenciadorDeAulas/Models/Usuario.cs
@@ -5,3 +5,4 @@ public class Usuario
     public string Nome { get; set; } = string.Empty;
     public DateTime DataAdmissao { get; set; }
 }
+// apenas um comentário para testar o git hub, não tem nada a ver com o código acima.
